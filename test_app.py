8f6f7bdbd8d02ca4844e6178fcf64759257117ad
Pitastic <mickep1985@gmail.com>
2023-07-14 23:25:49 +0200

    Clean UP after Tests

--- test_app.py
+++ test_app.py
@@ -2,7 +2,6 @@
 """Basisc Module for easy Imports and Methods"""
 
 import os
-import sys
 import cherrypy
 import pytest
 from cherrypy.test import helper
@@ -15,9 +14,8 @@ class TestClass(helper.CPWebCase):
     """Testen der Endpunkte und Basisfunktionen"""
 
     def setUp(self):
+        # Pass GC Test from CherryPy (will fail otherwise)
         self.do_gc_test = False
-        #cherrypy.engine.subscribe('stop', finally_delete_files)
-        #TODO: Delete Testfiles if any
 
     @staticmethod
     def setup_server():
@@ -25,16 +23,16 @@ class TestClass(helper.CPWebCase):
         config_path = os.path.join(
             os.path.dirname(os.path.abspath(__file__)),
             'testdata',
-            'test_config.conf'
+            'config.conf'
         )
         cherrypy.config.update(config_path)
         assert cherrypy.config.get('database.backend')
 
-        cherrypy.quickstart(UserInterface(), "/", config_path)
-        #cherrypy.tree.mount(UserInterface(), "/", config_path)
-        #cherrypy.engine.start()
-        #cherrypy.engine.block()
-    
+        cherrypy.tree.mount(UserInterface(), "/", config_path)
+        cherrypy.engine.start()
+        cherrypy.engine.exit()
+
+
     def test_index(self):
         """/index"""
         self.getPage("/")
@@ -51,14 +49,12 @@ class TestClass(helper.CPWebCase):
         self.assertStatus('200 OK')
 
 
-@pytest.fixture(scope='module', autouse=False)
-def finally_delete_files(request):
-    """Clean Up Testfiles"""
-    #TODO: Server muss beim Shutdown die Datei (aktiv) freigeben (close Connection)
-    #def delete_db():
-    print("Deleting Database")
+@pytest.fixture(scope='session', autouse=True)
+def cleanup_testfiles():
+    """CleaUp"""
+    yield
+    print("CleanUp: Deleting Testdatabase....")
     os.remove(os.path.join(
             cherrypy.config['database.uri'],
             cherrypy.config['database.name']
     ))
-    #request.addfinalizer(delete_db)
