a84061231b3d86f0308256b665633e0cac216b0c
Pitastic <mickep1985@gmail.com>
2026-04-29 20:41:19 +0200

    TinyDB muss für Git Wokflows default sein

--- tests/config.py
+++ tests/config.py
@@ -12,13 +12,13 @@ LOG_ERROR_FILE = '/tmp/pynance_error.log'
 PASSWORD = os.getenv('AUTH_PASSWORD', 'change_this_password')
 
 # - Database Backend ('tiny' or 'mongo')
-DATABASE_BACKEND = 'mongo'
-#DATABASE_BACKEND = 'tiny'
+#DATABASE_BACKEND = 'mongo'
+DATABASE_BACKEND = 'tiny'
 
-DATABASE_URI = 'mongodb://testuser:testpassword@localhost:27017' # For mongo (URI)
-#DATABASE_URI = '/tmp/pynance-test' # For tiny (/path/to/)
+#DATABASE_URI = 'mongodb://testuser:testpassword@localhost:27017' # For mongo (URI)
+DATABASE_URI = '/tmp/pynance-test' # For tiny (/path/to/)
 
 # For tiny: Filename ('testdata.json')
 # For mongo: Collection name ('testdata')
-DATABASE_NAME = 'testdata'
-#DATABASE_NAME = 'testdata.json'
+#DATABASE_NAME = 'testdata'
+DATABASE_NAME = 'testdata.json'
